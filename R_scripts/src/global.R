b274c0a44409fcf8a0e8a3978a559d317e422fc3
fmveranoc <fmveranoc@unal.edu.co>
2020-06-12 23:42:41 -0500

    Prueba de primer commit (Mathias)

---
+++ R_scripts/src/global.R
@@ -0,0 +1,46 @@
+# Loading libraries -------------------------------------------------------
+
+library(tidyverse)
+library(lubridate)
+library(tidymodels)
+
+
+# Configuring enviroment work and defining path parameters ----------------
+
+setwd('D:/2_Documentos Mathías/DS4A/Final_Project')
+
+dataPath = 'Data'
+
+# Loading dataset ---------------------------------------------------------
+
+dataset = file.path(dataPath) %>% 
+  list.files(full.names = T) %>% 
+  set_names( 
+    str_remove_all(string = list.files(path = file.path(dataPath)), pattern = '.xlsx') %>%  
+      iconv(from = 'UTF-8', to = 'ASCII//TRANSLIT') %>% 
+      str_to_upper %>% 
+      str_replace_all(pattern = '\\s+', '_')
+  ) %>% 
+  map(readxl::read_xlsx) %>% 
+  map(
+    ~ {
+      .x %>% 
+        rename_all(
+          .funs = ~ {
+            .x %>% 
+              iconv(from = 'UTF-8', to = 'ASCII//TRANSLIT') %>% 
+              str_to_upper %>% 
+              str_replace_all(pattern = '\\s+', '_')
+          }
+        ) %>% 
+        rename_at(
+          .vars = vars( contains("NUMERO_IDENTIFICACION"), contains("IDENTIFICACION") ), 
+          .funs = ~ "ID"
+        )
+    }
+  )
+
+
+
+
+
